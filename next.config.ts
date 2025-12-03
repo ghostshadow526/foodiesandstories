170f9677d4016f5b7baa138c9dfc3d87e995e308
Amin H. Nasser <aminhnasser876@gmail.com>
2025-12-03 19:10:47 +0000

    USE THIS AS THE logo https://github.com/ghostshadow526/CRUISESHIP/blob/m

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
-import { BookMarked } from 'lucide-react';
+import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2", className)}>
-      <BookMarked className="h-7 w-7 text-primary" />
+      <Image src="https://raw.githubusercontent.com/ghostshadow526/CRUISESHIP/main/ICE.png" alt="ICESTORES Logo" width={36} height={36} className="h-9 w-9" />
       <span className="font-headline text-2xl font-bold tracking-tight">
         ICESTORES
       </span>
