bdc4fa8be30745a7001498e5dda177d0899cc890
Amin H. Nasser <aminhnasser876@gmail.com>
2025-12-03 19:14:56 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -10,7 +10,8 @@ export default function Logo({ className }: { className?: string }) {
         alt="ICESTORES Logo" 
         width={40} 
         height={40} 
-        className="h-10 w-10 drop-shadow-lg" 
+        className="h-10 w-10 drop-shadow-lg"
+        priority
       />
       <span className="font-headline text-2xl font-bold tracking-tight">
         ICESTORES
